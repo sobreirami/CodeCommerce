3e12bf0074139fb6f3a81241f2f3b4285c1a0770
Michel Sobreira <sobreira.michel@gmail.com>
2015-05-18 09:12:56 -0300

    Primeiro commit

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,33 @@
+<?php namespace CodeCommerce\Http\Controllers;
+
+class WelcomeController extends Controller {
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+
+	/**
+	 * Show the application welcome screen to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return view('welcome');
+	}
+
+    public function exemplo()
+    {
+        $nome = "Michel";
+        $sobrenome = "Sobreira";
+
+        return view('exemplo', compact('nome','sobrenome'));
+    }
+
+}
---
+++ resources/views/exemplo.blade.php
@@ -0,0 +1,3 @@
+<h1>Exemplo</h1>
+
+<p>Olá {{ $nome . ' ' . $sobrenome }}</p>
