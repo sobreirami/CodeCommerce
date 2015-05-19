3e1bf467cf710f1929354ab1a7afde48d1ec75cf
Michel Sobreira <sobreira.michel@gmail.com>
2015-05-19 13:55:28 -0300

    Adicionando routes

---
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -0,0 +1,97 @@
+<?php namespace CodeCommerce\Http\Controllers;
+
+use CodeCommerce\Category;
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+class AdminCategoriesController extends Controller {
+
+    protected $categories;
+
+    public function __construct(Category $categories)
+    {
+
+        $this->categories = new Category();
+
+    }
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+        $categories = $this->categories->all();
+
+        return View('categories', compact('categories'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
